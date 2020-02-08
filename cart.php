40f845ff2f5f94047816c5e9258d2ad1582fd4d3
cohki0305 <cohki0305@gmail.com>
2020-02-08 17:35:52 +0900

    追加された個数を表示

--- cart.php
+++ cart.php
@@ -1,3 +1,5 @@
+<?php require_once 'products.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,50 +12,21 @@
   <div class="container">
     <div class="app-container">
       <h1 class="title">Shopping Cart</h1>
-        <div class="carts-container">
-          <div class="cart-item">
-            <div class="flex">
-              <img class="cart-item-img" src="./images/image_1.jpg">
-              <div class="cart-item-detail">
-                <p class="cart-item-title">アボカド</p>
-                <p>2 × 250円</p>
-              </div>
-            </div>
-          </div>
-          <div class="cart-item">
-            <div class="flex">
-              <img class="cart-item-img" src="./images/image_2.jpg">
-              <div class="cart-item-detail">
-                <p class="cart-item-title">オレンジ</p>
-                <p>2 × 250円</p>
-              </div>
-            </div>
-          </div>
-          <div class="cart-item">
-            <div class="flex">
-              <img class="cart-item-img" src="./images/image_3.jpg">
-              <div class="cart-item-detail">
-                <p class="cart-item-title">レモン</p>
-                <p>2 × 250円</p>
-              </div>
-            </div>
-          </div>
-          <div class="cart-item">
-            <div class="flex">
-              <img class="cart-item-img" src="./images/image_4.jpg">
-              <div class="cart-item-detail">
-                <p class="cart-item-title">パイナップル</p>
-                <p>2 × 250円</p>
-              </div>
+      <div class="carts-container">
+        <?php foreach($products as $product): ?>
+        <div class="cart-item">
+          <div class="flex">
+            <img class="cart-item-img" src="<?php echo $product["image"]; ?>">
+            <div class="cart-item-detail">
+              <p class="cart-item-title"><?php echo $product["name"]; ?></p>
+              <p><?php echo $_POST[$product["id"]]; ?> × 250円</p>
             </div>
           </div>
         </div>
+        <?php endforeach; ?>
       </div>
       <div class="btn-footer bg-gray">
-        <div class="checkout-btn flex justify-between">
-          <p>決済する</p>
-          <p>2000円</p>
-        </div>
+        <input class="checkout-btn flex justify-between" value="決済する">
       </div>
     </div>
   </div>
