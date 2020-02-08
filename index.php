4ba5e396646c8205248c18ce30970becc89b9c30
cohki0305 <cohki0305@gmail.com>
2020-02-08 16:08:34 +0900

    別ファイルに分けた

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once 'products.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,30 +13,6 @@
     <div class="app-container">
       <h1 class="title">DailyTrial Shopping</h1>
       <div class="cards-container">
-        <?php
-          $products = [
-            [
-              "name" => "アボカド",
-              "price" => 230,
-              "image" => "./images/image_1.jpg"
-            ],
-            [
-              "name" => "オレンジ",
-              "price" => 100,
-              "image" => "./images/image_2.jpg"
-            ],
-            [
-              "name" => "レモン",
-              "price" => 500,
-              "image" => "./images/image_3.jpg"
-            ],
-            [
-              "name" => "パイナップル",
-              "price" => 900,
-              "image" => "./images/image_4.jpg"
-            ],
-          ];
-        ?>
       <?php foreach($products as $product): ?>
       <div class="card">
         <img class="card-image" src="<?php echo $product["image"] ?>" alt="">
---
+++ products.php
@@ -0,0 +1,24 @@
+<?php
+  $products = [
+    [
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./images/image_1.jpg"
+    ],
+    [
+      "name" => "オレンジ",
+      "price" => 100,
+      "image" => "./images/image_2.jpg"
+    ],
+    [
+      "name" => "レモン",
+      "price" => 500,
+      "image" => "./images/image_3.jpg"
+    ],
+    [
+      "name" => "パイナップル",
+      "price" => 900,
+      "image" => "./images/image_4.jpg"
+    ],
+  ];
+?>
