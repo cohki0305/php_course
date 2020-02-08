34f6ebfeae6b75710bfe35f0992e3233b888450f
cohki0305 <cohki0305@gmail.com>
2020-02-08 15:33:56 +0900

    初期状態

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style.css" >
+  <title>デイトラ PHP</title>
+</head>
+<body>
+  <div class="container">
+    <div class="app-container">
+      <h1 class="title">DailyTrial Shopping</h1>
+      <div class="cards-container">
+        <div class="card">
+          <img class="card-image" src="./images/image_1.jpg" alt="">
+          <p class="card-title">アボカド</p>
+          <div class="flex justify-between">
+            <p class="card-price">230円</p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+      </div>
+      <div class="btn-footer">
+        <div class="cart-btn">
+          カートに追加
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
