b95f27dfff980890ef6e4200d347d5a29540b2ea
cohki0305 <cohki0305@gmail.com>
2020-02-09 10:18:49 +0900

    private について

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <?php
   require_once 'product.php';
   $banana = new Product('banana');
-  $banana->name = 'apple';
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +16,7 @@
 <body>
   <div class="container">
     <div class="app-container">
-      <h1 class="title">DailyTrial Shopping <?php echo $banana->name ?></h1>
+      <h1 class="title">DailyTrial Shopping <?php echo $banana->getName() ?></h1>
       <form method="post" action="cart.php">
         <div class="cards-container">
         <?php foreach($products as $product): ?>
--- product.php
+++ product.php
@@ -1,9 +1,13 @@
 <?php
   // Class というのは処理や値をまとまりにしたもの
   class Product {
-    public static $name;
+    private $name; // private にするとインスタンスからアクセスできない
     public function __construct($name) {
       $this->name = $name;
+    }
+
+    public function getName() {
+      return $this->name;
     }
   }
   // construct は class から インスタンス を作るときに使う
