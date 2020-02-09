8f4978cdb9e190888f2e1eb1eccb1a55d79137db
cohki0305 <cohki0305@gmail.com>
2020-02-09 10:26:23 +0900

    function をたくさん定義

--- product.php
+++ product.php
@@ -1,13 +1,32 @@
 <?php
   // Class というのは処理や値をまとまりにしたもの
   class Product {
+    private $id;
     private $name; // private にするとインスタンスからアクセスできない
-    public function __construct($name) {
+    private $price;
+    private $image;
+
+    public function __construct($id, $name, $price, $image) {
+      $this->id = $id;
       $this->name = $name;
+      $this->price = $price;
+      $this->image = $image;
     }
 
     public function getName() {
       return $this->name;
+    }
+
+    public function getPrice() {
+      return $this->price;
+    }
+
+    public function getImage() {
+      return $this->image;
+    }
+
+    public function getId() {
+      return $this->id;
     }
   }
   // construct は class から インスタンス を作るときに使う
